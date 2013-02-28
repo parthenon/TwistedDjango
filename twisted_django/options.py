b220e296fd9e60868150bb415ccbcd1ff9fbd815
Thomas Schreiber <ubiquill@gmail.com>
2013-02-27 16:07:24 -0800

    Changed how optons are input

--- twisted_django/options.py
+++ twisted_django/options.py
@@ -4,19 +4,19 @@ from optparse import OptionParser
 
 
 def get_options():
-    parser = OptionParser()
-    parser.add_option("-n", "--projectname", dest="project",
-                      help="Name of project", default=None)
-    parser.add_option("-p", "--port", dest="port",
-                      help="Port number", default="8080")
-    parser.add_option("-f", "--path", dest="path",
-                      help="The full path of the django project")
-    parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False)
-    (options, args) = parser.parse_args()
-
     if 'TWISTED_DJANGO_OPTIONS' in os.environ:
         options = os.environ['TWISTED_DJANGO_OPTIONS']
     else:
+        parser = OptionParser()
+        parser.add_option("-n", "--projectname", dest="project",
+                        help="Name of project", default=None)
+        parser.add_option("-p", "--port", dest="port",
+                        help="Port number", default="8080")
+        parser.add_option("-f", "--path", dest="path",
+                        help="The full path of the django project")
+        parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False)
+        (options, args) = parser.parse_args()
+
         if not options.project:
             print "Error: no project name specified"
             sys.exit(1)
