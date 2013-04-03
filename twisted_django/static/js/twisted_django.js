1369ff3d9c45d2b6e8d15054a073d388b52f09d9
Andrew Kettel <andrew@parthenonsoftware.com>
2013-04-03 09:57:28 -0700

    Added javascript functions that IE8 does not support

--- twisted_django/static/js/twisted_django.js
+++ twisted_django/static/js/twisted_django.js
@@ -1,6 +1,78 @@
 // connex/twisted_django_server/twisted_django.js
 // jslint browser: true
 
+//http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation
+if (!Object.keys) {
+    Object.keys = Object.keys || (function () {
+        var hasOwnProperty = Object.prototype.hasOwnProperty,
+            hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+            DontEnums = [
+                'toString',
+                'toLocaleString',
+                'valueOf',
+                'hasOwnProperty',
+                'isPrototypeOf',
+                'propertyIsEnumerable',
+                'constructor'
+            ],
+            DontEnumsLength = DontEnums.length;
+
+        return function (o) {
+            if (typeof o != "object" && typeof o != "function" || o === null)
+                throw new TypeError("Object.keys called on a non-object");
+
+            var result = [];
+            for (var name in o) {
+                if (hasOwnProperty.call(o, name))
+                    result.push(name);
+            }
+
+            if (hasDontEnumBug) {
+                for (var i = 0; i < DontEnumsLength; i++) {
+                    if (hasOwnProperty.call(o, DontEnums[i]))
+                        result.push(DontEnums[i]);
+                }
+            }
+
+            return result;
+        };
+    })();
+}
+
+//https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/IndexOf
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this === null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 1) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n !== 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    };
+}
+
 sock = function(wsuri, debug) {
     window.WEB_SOCKET_DEBUG = true;
     window.WEB_SOCKET_SWF_LOCATION ="/static/web-socket/WebSocketMain.swf";
@@ -98,7 +170,7 @@ sock = function(wsuri, debug) {
         is_connected = function() {
             return connected;
         },
-        onready = function(func) {                                                                                                                                                                                     
+        onready = function(func) {
             init_funcs.push(func);
         },
         on = function(key, listener, once) {
