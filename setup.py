101bdac4b66d30dce81d0ee4f43fc3906b32078e
Andy Schmitt <andy@parthenonsoftware.com>
2013-08-30 11:29:28 -0700

    Added use auth back in, and made socket something that can be reassigned (aka made non-global).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djangotwisted',
-    version='0.1.7',
+    version='0.1.8',
     description='Reusable websockets with Twisted in Django',
     author='Casey Beach',
     author_email='casey@parthenonsoftware.com',
